7505574f3810c7e4700acd5300841c88312beb9d
Leonid Lugovoi <yotohana.dev@gmail.com>
2025-12-04 15:14:26 +0500

    feat(database): create wallet table migration

---
+++ db/migrations/20251204100749_create_wallet_table.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS wallets(
+    wallet_uuid UUID PRIMARY_KEY,
+    balance INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS wallets;
+-- +goose StatementEnd
