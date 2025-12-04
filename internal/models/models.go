edab55dd5734b34faed2bb52a1b0ea9761bf88c7
Leonid Lugovoi <yotohana.dev@gmail.com>
2025-12-04 17:08:35 +0500

    feat(models): add models

---
+++ internal/models/models.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Wallets struct {
+	WalletUUID uuid.UUID `gorm:"primaryKey;column:wallet_id;type:uuid"`
+	Balance int `gorm:"column:balance;type:integer;not null"`
+	CreatedAt time.Time `gorm:"column:created_at;type:timestamp;default:CURRENT TIMESTAMP"`
+	UpdatedAt time.Time `gotm:"column:updated_at;type:timestamp"`
+}
